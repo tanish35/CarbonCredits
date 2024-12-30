8d5d39743500dda06e5a8102ca21a9d4309fecbf
tanish35 <tanishmajumdar2912@gmail.com>
2024-12-30 16:54:18 +0530

    Minor changes

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -2,13 +2,13 @@ import { http, createConfig } from "wagmi";
 import { avalancheFuji } from "wagmi/chains";
 import { coinbaseWallet, injected, metaMask } from "wagmi/connectors";
 
-const avalancheFujiRpcUrl = "https://api.avax-test.network/ext/bc/C/rpc";
+// const avalancheFujiRpcUrl = "https://api.avax-test.network/ext/bc/C/rpc";
 
 export const config = createConfig({
   chains: [avalancheFuji],
   connectors: [injected(), coinbaseWallet(), metaMask()],
   transports: {
-    [avalancheFuji.id]: http(avalancheFujiRpcUrl),
+    [avalancheFuji.id]: http(),
   },
 });
 
