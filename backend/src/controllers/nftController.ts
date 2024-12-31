1082e4dd49b3dba80bc604cfdbe2e1b02031912c
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-12-31 20:57:50 +0530

    Add get and create NFT

--- backend/src/controllers/nftController.ts
+++ backend/src/controllers/nftController.ts
@@ -44,3 +44,106 @@ export const sellNFT = asyncHandler(async (req: Request, res: Response) => {
     })
 
 });
+
+export const NFTtrasactions = asyncHandler(async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {buyerId, sellerId, nftId} = req.body;
+    if (!buyerId || !sellerId || !nftId) {
+        res.status(400).json({ message: "Please provide all the required fields" });
+        return;
+    }
+    const transaction = await prisma.transaction.create({
+        // @ts-ignore
+        data: {
+            buyer: {
+                connect: {
+                    id: buyerId,
+                },
+            },
+            nft: {
+                connect: {
+                    id: nftId,
+                },
+            },
+        },
+    })
+    const user = await prisma.user.update({
+        where: {
+            id: sellerId,
+        },
+        data: {
+            nfts: {
+                disconnect: {
+                    id: nftId,
+                },
+            },
+        },
+    })
+
+    const user2 = await prisma.user.update({
+        where: {
+            id: buyerId,
+        },
+        data: {
+            nfts: {
+                connect: {
+                    id: nftId,
+                },
+            },
+        },
+    });
+    res.json(transaction);
+});
+
+export const getNFTs = asyncHandler(async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const user = await prisma.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            nfts: true,
+        },
+    });
+    res.json(user);
+});
+
+export const getNFT = asyncHandler(async (req: Request, res: Response) => {
+    const {nftId} = req.body;
+    if (!nftId) {
+        res.status(400).json({ message: "Please provide an NFT id" });
+        return;
+    }
+    const nft = await prisma.nFT.findUnique({
+        where: {
+            id: nftId,
+        },
+    });
+    res.json(nft);
+});
+
+export const createNFT = asyncHandler(async (req: Request, res: Response) => {
+    const {tokenId, tokenURI, ownerId, price, createdAt, creditType, quantity, expiryDate} = req.body;
+    if (!tokenId || !tokenURI || !ownerId || !price || !createdAt || !creditType || !quantity || !expiryDate) {
+        res.status(400).json({ message: "Please provide all the required fields" });
+        return;
+    }
+    const nft = await prisma.nFT.create({
+        data: {
+            tokenId,
+            tokenURI,
+            ownerId,
+            price,
+            createdAt,
+            creditType,
+            quantity,
+            expiryDate,
+            owner: {
+                connect: {
+                    id: ownerId,
+                }
+            }
+        },
+    });
+    res.json(nft);
+});
--- backend/src/route/nftRoutes.ts
+++ backend/src/route/nftRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import requireAuth from '../milddleware/checkAuth';
-import { buyNFT, sellNFT } from '../controllers/nftController';
+import { buyNFT, createNFT, getNFT, getNFTs, NFTtrasactions, sellNFT } from '../controllers/nftController';
 
 const NFTrouter = express.Router();
 
 NFTrouter.put('/buy', requireAuth,buyNFT);
 NFTrouter.put('/sell', requireAuth,sellNFT);
+NFTrouter.put('/transactions', requireAuth, NFTtrasactions);
+NFTrouter.get('/getNFT', requireAuth, getNFT);
+NFTrouter.get('/getNFTs', requireAuth, getNFTs);
+NFTrouter.post('/createNFT', requireAuth, createNFT);
 export default NFTrouter;
