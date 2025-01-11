7204b95960ca7e9e8c677dd85b0bb4caaf860c51
dasakash26 <akashd2664@gmail.com>
2025-01-11 10:47:32 +0530

    Update CORS configuration and modify API URL handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://carboncredits-ug6q.onrender.com/",
+    ],
     credentials: true,
   })
 );
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 //axios config file
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL 
+//|| 'http://localhost:3001';
 
 export const api = axios.create({
   baseURL: API_URL,
